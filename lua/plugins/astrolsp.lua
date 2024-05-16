ecce9cfa2163d0dfb32eaefb8b4424af3b959e42
Max Machon <max@machon.family>
2024-05-16 10:58:45 -0400

    minor config changes

--- lua/plugins/astrolsp.lua
+++ lua/plugins/astrolsp.lua
@@ -1,3 +1,5 @@
+if true then return {} end
+
 -- AstroLSP allows you to customize the features in AstroNvim's LSP configuration engine
 -- Configuration documentation can be found with `:h astrolsp`
 -- NOTE: We highly recommend setting up the Lua Language Server (`:LspInstall lua_ls`)
