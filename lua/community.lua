e0d7283047fdbadcf6270c95ee56159ffe0d2cf2
Pragma8123 <max@machon.family>
2025-05-28 11:17:36 -0400

    remove toml community pack

--- lua/community.lua
+++ lua/community.lua
@@ -24,7 +24,6 @@ return {
   { import = "astrocommunity.pack.templ" },
   { import = "astrocommunity.pack.yaml" },
   { import = "astrocommunity.pack.zig" },
-  { import = "astrocommunity.pack.toml" },
   { import = "astrocommunity.pack.rust" },
   { import = "astrocommunity.pack.cs" },
   { import = "astrocommunity.pack.helm" },
