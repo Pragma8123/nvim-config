8d45b107991f79279de5fe5448be9cad01493810
Max Machon <max@machon.family>
2025-07-01 11:13:43 -0400

    remove some mason auto-installs

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -9,12 +9,10 @@ return {
     opts = function(_, opts)
       -- add more things to the ensure_installed table protecting against community packs modifying it
       opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, {
-        "htmx",
         "intelephense",
         "lua_ls",
         "rust_analyzer",
         "texlab",
-        "volar",
         -- add more arguments for adding more language servers
       })
     end,
