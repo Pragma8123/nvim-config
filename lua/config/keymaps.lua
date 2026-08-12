caeb5d40fa0b4f8a2d0dc217b1a672b9d8f84c45
Max Machon <max@machon.family>
2026-08-12 14:42:01 -0400

    fix: actually fix tmux tile/window navigation

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,11 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+vim.keymap.set("n", "<C-h>", "<cmd>NvimTmuxNavigateLeft<cr>", { desc = "Navigate Left" })
+vim.keymap.set("n", "<C-j>", "<cmd>NvimTmuxNavigateDown<cr>", { desc = "Navigate Down" })
+vim.keymap.set("n", "<C-k>", "<cmd>NvimTmuxNavigateUp<cr>", { desc = "Navigate Up" })
+vim.keymap.set("n", "<C-l>", "<cmd>NvimTmuxNavigateRight<cr>", { desc = "Navigate Right" })
+vim.keymap.set("n", "<C-\\>", "<cmd>NvimTmuxNavigateLastActive<cr>", { desc = "Navigate Last Active" })
+
+
--- lua/plugins/tmux.lua
+++ lua/plugins/tmux.lua
@@ -1,19 +1,28 @@
 return {
   {
     "alexghergh/nvim-tmux-navigation",
+    cmd = {
+      "NvimTmuxNavigateLeft",
+      "NvimTmuxNavigateDown",
+      "NvimTmuxNavigateUp",
+      "NvimTmuxNavigateRight",
+      "NvimTmuxNavigateLastActive",
+      "NvimTmuxNavigateNext",
+    },
+    keys = {
+      { "<C-h>", "<cmd>NvimTmuxNavigateLeft<cr>", desc = "Navigate Left" },
+      { "<C-j>", "<cmd>NvimTmuxNavigateDown<cr>", desc = "Navigate Down" },
+      { "<C-k>", "<cmd>NvimTmuxNavigateUp<cr>", desc = "Navigate Up" },
+      { "<C-l>", "<cmd>NvimTmuxNavigateRight<cr>", desc = "Navigate Right" },
+      { "<C-\\>", "<cmd>NvimTmuxNavigateLastActive<cr>", desc = "Navigate Last Active" },
+    },
     config = function()
-      local nvim_tmux_nav = require("nvim-tmux-navigation")
-
-      nvim_tmux_nav.setup({
-        disable_when_zoomed = true, -- defaults to false
-        keybindings = {
-          left = "<C-h>",
-          down = "<C-j>",
-          up = "<C-k>",
-          right = "<C-l>",
-          last_active = "<C-\\>",
-        },
+      require("nvim-tmux-navigation").setup({
+        disable_when_zoomed = true,
       })
     end,
   },
 }
+
+
+
